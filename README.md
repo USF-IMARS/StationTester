01ca2e0dc14cad8519b5151ba5ccd9c1afe097cc
Tylar Murray <murray.tylar@gmail.com>
2017-02-28 21:19:34 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Installation:
+
+`python setup.py install`
+or
+`python setup.py develop`
+
+Example usage:
+
+```
+from StationTester.TestHelper import TestHelper
+```
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import io
+
+import StationTester
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md') #, 'CHANGES.txt')
+
+setup(name='StationTester',
+    version=StationTester.__version__,
+    description='IPOPP Station Testing Framework',
+    long_description=long_description,
+    author='Tylar Murray',
+    author_email='tylarmurray@mail.usf.edu',
+    url='https://github.com/USF-IMARS/station-tester',
+
+    tests_require=['nose'],
+    install_requires=[
+    ],
+    #cmdclass={'test': PyTest},
+
+    packages=['StationTester']
+)
