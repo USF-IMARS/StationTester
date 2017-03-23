8aca70efcf32962bf56ce241d66d5c247ed63162
Tylar Murray <murray.tylar@gmail.com>
2017-03-23 20:30:46 +0000

    add ignore packages/stations via config file

--- StationTester/Grapher.py
+++ StationTester/Grapher.py
@@ -5,6 +5,7 @@ tests station graphing functionality
 
 import argparse
 from enum import Enum
+import configparser
 
 import networkx as netx
 
@@ -14,9 +15,11 @@ from StationTester import path_helper, util
 NodeType = Enum("NodeType", "STATION PRODUCT")
 
 class Grapher(object):
-    def __init__(self, verbocity=0):
+    def __init__(self, verbocity=0, graphignore=None):
         self.verbose = (verbocity > 0)
         self.verbocity = verbocity
+        self._set_ignore_file(graphignore)
+
         self.graph = netx.DiGraph()
 
     def graph_all(self, varsub=True):
@@ -31,46 +34,59 @@ class Grapher(object):
         """
         produces graph of in/outfows for all stations in given SPA
         """
-        for stationName in util.get_stations(packageName):
-            self.graph_station(packageName, stationName, varsub=varsub)
+        if (self._ignores(packageName)):
+            if self.verbose: print("ignoring p: " + packageName)
+            return
+        else :
+            for stationName in util.get_stations(packageName):
+                self.graph_station(packageName, stationName, varsub=varsub)
 
     def graph_station(self, packageName, stationName, varsub=True):
         """
         produces graph of given station in/outflows
         """
-        if self.verbose: print("graphing "+packageName+"/"+stationName+"...")
-        cfgfile = CFGFileReader(
-            path_helper.cfg_path(packageName, stationName),
-            verbose=(self.verbocity > 1)
-        )
-        cfgfile.set_varsub(varsub)
+        if self._ignores(packageName, station=stationName):
+            if self.verbose: print("ignoring s: " + packageName + "/" + stationName)
+            return
+        else:
+            if self.verbose: print("graphing "+packageName+"/"+stationName+"...")
+            cfgfile = CFGFileReader(
+                path_helper.cfg_path(packageName, stationName),
+                verbose=(self.verbocity > 1)
+            )
+            cfgfile.set_varsub(varsub)
 
-        self._add_node(stationName, NodeType.STATION, cfgfile)
+            self._add_node(stationName, NodeType.STATION, cfgfile, package=packageName)
 
-        # connect inflow product to station
-        for inflow in cfgfile.get_inflows():
-            if self.verbose: print(inflow, "=>", stationName)
-            self._add_node(inflow, NodeType.PRODUCT, cfgfile)
-            self._add_edge(inflow, stationName)
+            # connect inflow product to station
+            for inflow in cfgfile.get_inflows():
+                if self.verbose: print(inflow, "=>", stationName)
+                self._add_node(inflow, NodeType.PRODUCT, cfgfile)
+                self._add_edge(inflow, stationName)
 
-        # connect station to outflow products
-        for outflow in cfgfile.get_outflows():
-            if self.verbose: print(stationName, "=>", outflow)
-            self._add_node(outflow, NodeType.PRODUCT, cfgfile)
-            self._add_edge(stationName, outflow)
+            # connect station to outflow products
+            for outflow in cfgfile.get_outflows():
+                if self.verbose: print(stationName, "=>", outflow)
+                self._add_node(outflow, NodeType.PRODUCT, cfgfile)
+                self._add_edge(stationName, outflow)
 
     def save(self, filepath):
         """ saves graph at given filepath """
         netx.write_gexf(self.graph, filepath, version="1.2draft")
 
-    def _add_node(self, name, node_type, cfgfile):
-        attribs = {"size":1}
+    def _add_node(self, name, node_type, cfgfile, package="?"):
+        attribs = {
+            "size":1,
+            "package": package
+        }
         if node_type == NodeType.STATION:
             attribs["group"]=cfgfile.get_global_attrib("cfg_groupTag")
             attribs["version"]=cfgfile.get_global_attrib("version")
+            attribs["type"]="STATION"
         elif node_type == NodeType.PRODUCT:
             attribs["group"]="product"
             attribs["version"]="0.0.0"
+            attribs["type"]="PRODUCT"
             # TODO: check for other products matching wld.%.card values here
             # TODO: maybe w/ self.graph.nodes()?
         else:
@@ -89,6 +105,35 @@ class Grapher(object):
             {"Size":1}
         )
 
+    def _set_ignore_file(self, filename=None):
+        """
+        reads list of things to ignore from given filename and sets relevant
+        class atributes
+        """
+        if filename is None:
+            self._ignored_stations = []
+            self._ignored_packages = []
+        else:
+            cfg = configparser.ConfigParser()
+            cfg.read(filename)
+            statlist = cfg['grapher']['stations'].split(',')
+            packlist = cfg['grapher']['packages'].split(',')
+            self._ignored_stations = [ sss.strip() for sss in statlist ]
+            self._ignored_packages = [ sss.strip() for sss in packlist ]
+
+    def _ignores(self, package, station=None):
+        """
+        returns True if given package or station should be ignored
+        """
+        if package in self._ignored_packages:
+            return True
+        else:
+            if station is not None and station in self._ignored_stations:
+                return True
+            else:
+                return False
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='graph SPA station(s) in/outflows')
@@ -102,6 +147,9 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--nosub",
         help="do not use variable substitution when reading cfgfile(s)",
         action="store_true"
+    )
+    parser.add_argument("-c", "--config",
+        help="config file for excluding stations or SPAs"
     )
     parser.add_argument("outfile", help="file to save output")
 
